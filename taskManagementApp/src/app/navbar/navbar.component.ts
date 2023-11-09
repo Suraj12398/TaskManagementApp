d0ae87753cfd1b2c98100eb87eff7f03783d376c
SURAJ DEOSARKAR <surajdeosarkar@SURAJs-MacBook-Air.local>
2023-11-09 10:38:46 +0530

    someCssChangesDone

--- taskManagementApp/src/app/navbar/navbar.component.ts
+++ taskManagementApp/src/app/navbar/navbar.component.ts
@@ -1,13 +1,13 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
-
+import { Input } from '@angular/core';
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent {
-
+  @Input() showElement: boolean = true;
   constructor(private router: Router) {}
   showSuccessAlert: boolean = false;
   logout() {
